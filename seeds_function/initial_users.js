388e1e0b0f22efca2010b952b9e063512d0b2733
Imam Tauhid <imam.tauhid.dar@gmail.com>
2018-08-04 00:10:50 +0700

    update seeds user

--- seeds_function/initial_users.js
+++ seeds_function/initial_users.js
@@ -9,8 +9,9 @@ module.exports = function (knex) {
 	return knex('users')
 	.returning(['id'])
 	.insert([
-	  {id: 1, username: 'admin', email: 'imam@gmail.com', password: '$2a$10$etI9hTwl88piXMZQu22BGOPfZoEj/faq0Dj08frhyko3KfFLc/0xm'},
-	  {id: 2, username: 'kucingbelang4', email: 'kuc@rumaji.com', password: '$2a$10$etI9hTwl88piXMZQu22BGOPfZoEj/faq0Dj08frhyko3KfFLc/0xm'},
-	  {id: 3, username: 'ma4m', email: 'kel@rumaji.com', password: '$2a$10$etI9hTwl88piXMZQu22BGOPfZoEj/faq0Dj08frhyko3KfFLc/0xm'},
+	  {id: 1, username: 'admin', email: 'imam@gmail.com', password: '$2a$10$1xzhGT6QwLMiHGRYQZpfnOzAeOIR7/3IVyBAjsPnanTd8pEiCnqGi'},
+	  {id: 2, username: 'kucingbelang4', email: 'kuc@rumaji.com', password: '$2a$10$1xzhGT6QwLMiHGRYQZpfnOzAeOIR7/3IVyBAjsPnanTd8pEiCnqGi'},
+	  {id: 3, username: 'ma4m', email: 'kel@rumaji.com', password: '$2a$10$1xzhGT6QwLMiHGRYQZpfnOzAeOIR7/3IVyBAjsPnanTd8pEiCnqGi'},
+	  {id: 4, username: 'nooble', email: 'imam.tauhid.dar@gmail.com', password: '$2a$10$1xzhGT6QwLMiHGRYQZpfnOzAeOIR7/3IVyBAjsPnanTd8pEiCnqGi'},
 	])
 } 
