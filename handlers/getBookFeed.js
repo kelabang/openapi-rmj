2dc7911d370d0190419b37aead055c2e58424e8b
Imam <imam.tauhid.dar@icloud.com>
2018-09-10 00:13:28 +0700

    update

--- handlers/getBookFeed.js
+++ handlers/getBookFeed.js
@@ -23,9 +23,60 @@ exports.handler = function getBookFeed(req, res, next) {
 	const name = getNameCaller()
 	debug = Debug('rumaji:'+name)
 
-	function modelQuery () {}
-	function preResponseHandler () {}
-	function postResponseHandler () {}
+	function modelQuery () {
+		try {
+			debug('feed fetching')
+			const {
+				id,
+				mode
+			} = req.query
+
+			const limit = 15
+
+			let fn = '';
+			switch (mode) {
+				case 'next':
+					fn = 'feedMoreNext';
+					break;
+				case 'prev':
+					fn = 'feedMorePrev';
+					break;
+				default:
+					fn = 'feed';
+			}
+			return Feeds[fn]({ id, limit })
+				.then(function (feeds) {
+					return feeds.map(function (feed) {
+						feed.set('user', feed.related('user'))
+						feed.set('comments', feed.related('comments'))
+						return feed
+					})
+				})
+
+		}
+		catch (err) {
+			debug('feed fetch failed')
+			console.error(err)
+			return false
+		}
+	}
+
+	function preResponseHandler (data) {
+		debug('pre-response handler')
+		let code = 200
+		if (typeof data == 'boolean' && !data) code = 503
+		return {
+			code,
+			data,
+			name
+		}
+	}
+
+	function postResponseHandler (data) {
+		debug('post-response handler')
+		res.send(data.code, data)
+		next()
+	}
 
 	const tasks = [
 		modelQuery,
